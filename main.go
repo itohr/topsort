34f412b41b61fe7d78e10b11e85b0a7a5a23881d
itohr <hiroki.ito.65.38@gmail.com>
2017-12-24 00:45:25 +0900

    Implemented Topological Sort

--- main.go
+++ main.go
@@ -8,25 +8,48 @@ import (
 )
 
 type Service struct {
-	Name      string   `json:"name"`
 	DependsOn []string `json:"depends_on"`
 }
 
-//func TopSort(services []Service){
-//	for _, service := range services {
-//	}
-//}
+type Node struct {
+	Services map[string]Service
+}
+
+func TopSort(services map[string]Service) []string {
+	var res []string
+	for name, _ := range services {
+		fmt.Printf("Check Name: %s\n", name)
+		CheckDependency(name, services, &res)
+	}
+	return res
+}
+
+func CheckDependency(name string, services map[string]Service, res *[]string) {
+	for _, v := range *res {
+		if v == name {
+			return
+		}
+	}
+	if services[name].DependsOn != nil {
+		for _, dep := range services[name].DependsOn {
+			CheckDependency(dep, services, res)
+		}
+	}
+	fmt.Printf("Add Service: %s\n", name)
+	*res = append(*res, name)
+}
 
 func main() {
-	bytes, err := ioutil.ReadFile("containers.json")
+	bytes, err := ioutil.ReadFile("services.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var services []Service
-	if err := json.Unmarshal(bytes, &services); err != nil {
+	var node Node
+	if err := json.Unmarshal(bytes, &node); err != nil {
 		log.Fatal(err)
 	}
-	for _, p := range services {
-		fmt.Printf("%s : %s\n", p.Name, p.DependsOn)
-	}
+
+	res := TopSort(node.Services)
+
+	fmt.Printf("Result: %s\n", res)
 }
